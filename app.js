8c2eb023d24f21f91636491fc60e1607fa7ccc13
javierleovalen <javier.leonardo.valencia@gmail.com>
2022-04-20 20:16:54 -0300

    agregando configuracion para heroku

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const publicPath = path.resolve(__dirname,'./public');
 
 app.use(express.static(publicPath));
                     
-app.listen(3000, ()=> console.log('Pedido exitoso en el puerto 3000'));
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
 
+    
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname,'./views/home.html'));
 })
